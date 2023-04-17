5cb1557078309b517b2d5c1d885e587917198d38
gxranger <1486899711@qq.com>
2023-04-17 09:45:05 +0800

    bug修复：解决左侧导航在部分移动设备上出现布局错乱问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     theme: {
         screens: {
-          'xs' : '375px',
+          'xs' : '360px',
           'sm': '640px',
           'md': '768px',
           'lg': '1024px',
